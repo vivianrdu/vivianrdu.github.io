bffcf714a7137b47f40421882fbfb1cd7047685c
Vivian <vivianruodu@gmail.com>
2022-05-11 23:29:42 -0700

    update

---
+++ js/addnavbar.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    var navitems = '';
+  
+    navitems += '<h1 class="first"><a href="index.html">home</a></h1><h1>' +
+           '<a href="about.html" >about</a></h1><h1>' +
+           '<a href="contact.html" >contact</a></h1>';
+  
+    $('#nav').append(navitems);
+    
+  }
+  );
