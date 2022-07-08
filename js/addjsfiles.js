24122d4f8da98b73131727e87e374bac20a89174
Vivian <vivianruodu@gmail.com>
2022-07-08 15:21:32 -0700

    font

--- js/addjsfiles.js
+++ js/addjsfiles.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     var jsitems = '';
   
     jsitems += '<script src="js/addnavbar.js"></script>' +
-           '<script src="js/addicons.js"></script>' +
            '<script src="js/addfooter.js"></script>';
   
     $('#js').append(jsitems);
